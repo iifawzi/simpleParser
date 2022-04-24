ce16cb85311b1fc8cfc2211e362fc8377ed85a78
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2022-04-24 21:37:09 +0200

    Adding the tokenizer class
    Signed-off-by: Fawzi E. Abdulfattah <iifawzie@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,10 +5,31 @@
 //  Created by Fawzi on 22/04/2022.
 //
 
+#include "Tokenizer.hpp"
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+using namespace simpleparser;
+
+int main() {
+        std::cout << "simpleparser 0.1\n" << endl;
+
+        FILE *fh = fopen("/Users/iifawzi/Desktop/Parser/test.myc", "r");
+        if (!fh) { cerr << "Can't find file." << endl; }
+        fseek(fh, 0, SEEK_END);
+        size_t fileSize = ftell(fh);
+        fseek(fh, 0, SEEK_SET);
+        string fileContents(fileSize, ' ');
+        fread(fileContents.data(), 1, fileSize, fh);
+
+        cout << fileContents << endl << endl;
+
+        Tokenizer tokenizer;
+        vector<Token> tokens = tokenizer.parse(fileContents);
+
+        for(Token currToken : tokens) {
+            currToken.debugPrint();
+        }
+
     return 0;
 }
